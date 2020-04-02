0a8539ce79abb1f39b09b7960f64d6de60ada28d
Shubham <smartshubhampathak@gmail.com>
2020-04-03 02:18:02 +0530

    Added comments and Sql file

--- DatabaseQueries.sql
+++ DatabaseQueries.sql
@@ -1,4 +1,12 @@
 use blogger;
+
+create table User(
+    user_id int auto_increment, 
+    username varchar(100), 
+    pass varchar(100),
+    primary key(user_id)
+);
+
 create table Blog(
 b_id int auto_increment,
 title text,
@@ -12,17 +20,11 @@ user_id int,
 foreign key(user_id) references User(user_id)
 );
 
-create table User(
-    user_id int auto_increment, 
-    username varchar(100), 
-    pass varchar(100),
-    primary key(user_id)
-);
 
-drop table Blog;
+-- Queries
 
 select * from Blog where b_id=1;
-drop table User;
+
 select * from Blog order by dateandTime desc;
 select * from User;
 INSERT INTO `blogger`.`Blog` (`b_id`, `title`, `postBody`, `photo`, `dateandTime`, `author`, `aboutAuthor`, `user_id`) VALUES ('1', 'nodejs', 'Perfect Framework its just the bestest bestsessfsdfsknjnsdkjbnfsbn askdbjnakjsfbn safbknafskjbnab', 'img.jpg', '2020-01-19 03:14:07', 'Shubham Pathak', 'Best developer', '1');
--- index.js
+++ index.js
@@ -9,19 +9,21 @@ var multer = require('multer')
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/uploads')
+      cb(null, './public/uploads')    
     },
     filename: function (req, file, cb) {
       cb(null, new Date().getTime()+"-"+req.session.user_id+file.originalname);
     }
   })
+
+
 var upload = multer({ storage: storage })
 const saltRounds = 10;
 
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static('public'));  //Serving static files from public folder like css and js files
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
@@ -36,6 +38,7 @@ app.use(session({
 
 //Home Route
 app.get('/', (req, res) => {
+    //fetching blogs in descending order of date so that recent post comes first
     connection.query('select * from Blog order by dateandTime desc;',(error,results,fields)=>{
         if(error) console.log(error);
         res.render('index.ejs',{posts:results});
@@ -52,12 +55,14 @@ app.route('/Login')
     .post((req, res) => {
         var query1 = 'select user_id,pass from User where username =?;';
         connection.query(query1, [req.body.userName], (error, results, fields) => {
+            //no such entry in the database
             if (results[0] == undefined)
                 res.render('login.ejs', { loginStatus: "Wrong Email or Password. Try Again! " });
 
             else if (error) throw error;
 
             else {
+                //entry found now comparing the passwords
                 bcrypt.compare(req.body.password, results[0].pass, function (err, result) {
                     // result == true
                     if (result == true) {
@@ -86,13 +91,16 @@ app.route('/Register')
     })
     .post((req, res) => {
         bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-            // Store hash in your password DB.
+            // Store hash in your DB.
             if (err) throw err;
             var query1 = 'insert into User (username,pass) values (?,?);';
             connection.query(query1, [req.body.userName, hash], (error, results, fields) => {
                 if (error) throw error;
                 console.log('Value inserted successfuly');
             });
+
+            //counting the number of users to get the uid of latest added user and storing it in the session variable
+            //there can be mush efficient way but this is what I thought of
             connection.query('SELECT count(*) as uid from user', function (error, results, fields) {
                 if (error) throw error;
                 req.session.user_id = results[0].uid;
@@ -105,7 +113,7 @@ app.route('/Register')
 
 
 //Articles Route
-app.use('/article/:b_id', express.static('public'));
+app.use('/article/:b_id', express.static('public')); //for serving static files at dynamic routes as its not by default
 app.get('/article/:b_id', (req, res) => {
     var b_id = req.params.b_id;
     connection.query('select * from Blog where b_id=?',[b_id],(error,results,fields)=>{
@@ -118,15 +126,19 @@ app.get('/article/:b_id', (req, res) => {
 app.delete('/article/:b_id',(req,res)=>{
     connection.query('select * from Blog where b_id=?',[req.params.b_id],(error,results,fields)=>{
         if(error)console.log(error);
+
+        //if someone else is trying to delete the post send unauthorized
         if(req.session.user_id!=results[0].user_id)
         {
             res.json({message:"Not Authorized"});
-            // res.render('article',{post:results[0],buttonStatus:"You're not Authorized"});
         }
         else{
+            //delete the row from database
             connection.query('DELETE FROM Blog WHERE b_id=?;',[req.params.b_id],(error,results1,fields)=>{
                 if(error) console.log(error);
                 console.log("Deleted the Post");
+
+                //Delete the file from uploads folder
                 fs.unlink('./public/uploads/'+results[0].photo, function (err) {
                     if (err) throw err;
                     // if no error, file has been deleted successfully
@@ -144,6 +156,7 @@ app.delete('/article/:b_id',(req,res)=>{
 //New Post Route
 app.route('/newPost')
     .get((req, res) => {
+        //Only logged in users can add new post
         if (req.session.user_id == null) {
             res.redirect('/Login');
         }
@@ -153,6 +166,7 @@ app.route('/newPost')
     })
     .post(upload.single('img'), (req, res) => {
         var date = new Date();
+        //timestamp is required as mysql support a particular format
         var timestamp = date.toISOString().slice(0, 10) + " " + date.toISOString().slice(11, 19);
         var query = "INSERT INTO Blog (title, postBody,photo,dateandTime,author,aboutAuthor,user_id) VALUES (?,?,?,?,?,?,?)";
         connection.query(query, [req.body.title, req.body.postBody,req.file.filename,timestamp,req.body.author,req.body.aboutAuthor,req.session.user_id], (error, results, fields) => {
